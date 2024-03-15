dcc4bb87d98d4b3ea2ee96ac64469c50c40d38a6
Krasnoshlik <dimakrasnoshluk@gmail.com>
2024-03-15 17:46:10 +0200

    feat: optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 const api = {
   key: "f9e92e0fdf338f8991e58ed830d7a097",
@@ -8,14 +8,40 @@ const api = {
 function App() {
   const [city, setCity] = useState('');
   const [weather, setWeather] = useState({});
-
+  const [day, setDay] = useState('');
+  const [time, setTime] = useState('');
+  const [img, setImg] = useState('./clouds.png')
 
   const searchPressed = () => {
     fetch(`${api.base}weather?q=${city}&units=metric&APPID=${api.key}`)
     .then((res) => res.json())
-    .then((data) => {setWeather(data)})
+    .then((result) => {setWeather(result)})
   }
 
+  function DayCalc() {
+    if(typeof weather.main !== 'undefined') {
+      const datatime = new Date(weather.dt * 1000).toLocaleString('default');
+      console.log(datatime)
+      const datatimeSplited = datatime.split(',');
+      const data = datatimeSplited[0];
+      const time = datatimeSplited[1];
+      setDay(data)
+      setTime(time)
+    } else {
+      return undefined
+    }
+  }
+
+  function WeatherImage() {
+    if(weather.weather[0].main === 'Clouds') {
+    setImg('./Sun and cloud.png')
+    }
+  }
+
+console.log(weather)
+  useEffect(() => {
+    DayCalc();
+  })
   return (
       <div className="container">
         <div className="left">
@@ -24,7 +50,7 @@ function App() {
           <button onClick={searchPressed} className="input-button"><img src="./search.png" width='100%' alt="search"/></button>
           </div>
           <div className="img-wrapper">
-            <img src="./Sun and cloud.png" alt="Sun and cloud" width='100%' />
+            <img src={img} alt="Sun and cloud" width='100%' onC/>
           </div>
 
           <div className="general-info">
@@ -35,9 +61,9 @@ function App() {
           <span className="middle-line"></span>
 
           <div className="more-info">
-            <h2>21-July-2023</h2>
-            <h2>Friday, 12:44 PM</h2>
-            <h2>Day</h2>
+            <h2>Date and time of request <br/> information:</h2>
+            <h2>{day.length > 0 ? (day) : '21-July-2023'}</h2>
+            <h2>{time.length > 0 ? (time) : '15:04'}</h2>
           </div>
 
 
@@ -46,49 +72,34 @@ function App() {
           <div className="info-grid">
 
             <div className="info">
-              <h2>Wind</h2>
+              <h2>Wind:</h2>
               <h1>{typeof weather.main !== 'undefined' ? (weather.wind.speed) : 6}km/h</h1>
               <h3> Degree : {typeof weather.main !== 'undefined' ? (weather.wind.deg) : 200}</h3>
             </div>
 
             <div className="info">
-              <h2>Humadity</h2>
+              <h2>Humadity:</h2>
               <h1>{typeof weather.main !== 'undefined' ? (weather.main.humidity) : 6}%</h1>
             </div>
 
             <div className="info">
-              <h2>Real Feel</h2>
+              <h2>Real Feel:</h2>
               <h1>{typeof weather.main !== 'undefined' ? (weather.main.feels_like) : 31 }°C</h1>
             </div>
 
             <div className="info">
-              <h2>Desctription:</h2>
-              <h1>{typeof weather.main !== 'undefined' ? (weather.weather[0].description) : 'Cloudy'}</h1>
+              <h2>Min temp:</h2>
+              <h1>{typeof weather.main !== 'undefined' ? (weather.main.temp_min) : 9}°C</h1>
             </div>
 
             <div className="info">
-              <h2>Pressure</h2>
-              <h1>{typeof weather.main !== 'undefined' ? (weather.main.pressure) : 1000 }mb</h1>
+              <h2>Pressure:</h2>
+              <h1>{typeof weather.main !== 'undefined' ? (weather.main.pressure) : 1000 } mb</h1>
             </div>
 
             <div className="info">
-              <h2>Change of rain</h2>
-              <h1>{typeof weather.main !== 'undefined' ? (weather.rain) : 70 }%</h1>
-            </div>
-
-            <div className="info">
-              <h2>Temperature History</h2>
-              <h1>31 °C</h1>
-            </div>
-
-            <div className="info">
-              <h2>Sun</h2>
-              <h1>Rise {typeof weather.main !== 'undefined' ? (new Date( weather.sys.sunrise * 1000).getHours()) : 5.12 } am</h1>
-            </div>
-
-            <div className="info">
-              <h2>Moon</h2>
-              <h1>Rise 5.17 am</h1>
+              <h2>Max temp:</h2>
+              <h1>{typeof weather.main !== 'undefined' ? (weather.main.temp_max) : 8}°C</h1>
             </div>
 
           </div>
